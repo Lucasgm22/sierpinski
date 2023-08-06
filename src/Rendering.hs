99fd18f1eb043a2c465ac3fbda8efa8fc4fd3dc9
lucas_lsgsma <lucas.guimaraes@qintess.com>
2023-08-06 12:41:19 -0300

    rendering

--- src/Rendering.hs
+++ src/Rendering.hs
@@ -2,4 +2,32 @@ module Rendering (
     worldAsPicture
 ) where
 
-worldAsPicture = undefined
+import Graphics.Gloss
+import World
+import Data.Array
+
+filledCell :: Picture
+filledCell = rectangleSolid cellWidth cellHeight
+
+snapPictureToCell picture (row, column) = translate x y picture
+  where x = fromIntegral column * cellWidth + cellWidth * 0.5
+        y = fromIntegral row * cellHeight + cellHeight * 0.5
+
+cellsOfBoard :: Board -> Cell -> Picture -> Picture
+cellsOfBoard board cell cellPicture =
+    pictures
+    $ map (snapPictureToCell cellPicture . fst)
+    $ filter (\(_, e) -> e == cell)
+    $ assocs board
+
+filledCellsOfBoard :: Board -> Picture
+filledCellsOfBoard board = cellsOfBoard board Filled filledCell
+
+pictureFilledCellOfBoard :: Board -> Picture
+pictureFilledCellOfBoard board = color white (filledCellsOfBoard board)
+
+worldAsPicture :: World -> Picture
+worldAsPicture world = translate (fromIntegral screenWidth * (-0.5))
+                                 (fromIntegral screenWidth * (-0.5))
+                                 frame
+    where frame = pictureFilledCellOfBoard (worldBoard world)
--- src/World.hs
+++ src/World.hs
@@ -3,27 +3,40 @@ module World (
     initialWorld
   , screenHeight
   , screenWidth
+  , cellHeight
+  , cellWidth
   , size
+  , World
+  , worldBoard
+  , Board
+  , Cell(..)
 ) where
 
 import Data.Array
 
-data Cell = Empty | Filled
+data Cell = Empty | Filled deriving (Eq, Show)
 
-type Board = Array (Int, Int) Cell
+type Board = Array (Int, Int) Cell 
 
 newtype World = World { worldBoard :: Board }
+
+cellWidth :: Float
+cellWidth = fromIntegral screenWidth / fromIntegral size
+
+cellHeight :: Float
+cellHeight = fromIntegral screenHeight / fromIntegral size
 
 screenWidth :: Int
 screenWidth = 640
 
 screenHeight :: Int
-screenHeight = 480
+screenHeight = 640
 
 size :: Int
 size = 100
 
 initialWorld :: World
-initialWorld = World { worldBoard = array indexRange $ map (, Empty) (range indexRange)}
+initialWorld = World { worldBoard = (array indexRange $ map (, Empty) (range indexRange)) // [ ((0, 0), Filled), ((1, 1), Filled) ]
+                     }
   where
     indexRange = ((0, 0), (size - 1, size - 1))
