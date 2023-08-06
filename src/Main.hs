1ae07ec0a30a4413a6e957331465c9979938da97
lucas_lsgsma <lucas.guimaraes@qintess.com>
2023-08-06 12:45:57 -0300

    fps

--- src/Main.hs
+++ src/Main.hs
@@ -13,5 +13,8 @@ window = InWindow "Name of the Window" (screenWidth, screenHeight) (100, 100)
 backgroundColor :: Color
 backgroundColor = makeColor 0 0 0 255
 
+fps :: Int
+fps = 30
+
 main :: IO ()
-main = play window backgroundColor 30 initialWorld worldAsPicture transformWorld animateWorld
+main = play window backgroundColor fps initialWorld worldAsPicture transformWorld animateWorld
